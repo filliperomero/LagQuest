a402ae45eedad4c638a94a004b63ef69cee59009
filliperomero <fillipe.romero@gmail.com>
2025-08-14 22:08:58 -0600

    Add LAN Menu, add Lobby Map

---
+++ Source/LagQuest/Private/LAN/Lq_LANMenu.cpp
@@ -0,0 +1,39 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#include "Public/LAN/Lq_LANMenu.h"
+
+#include "Components/Button.h"
+#include "Components/EditableTextBox.h"
+#include "Kismet/GameplayStatics.h"
+
+void ULq_LANMenu::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	FInputModeUIOnly InputMode;
+	GetOwningPlayer()->SetInputMode(InputMode);
+	GetOwningPlayer()->SetShowMouseCursor(true);
+
+	Button_Host->OnClicked.AddDynamic(this, &ThisClass::HostButtonClicked);
+	Button_Join->OnClicked.AddDynamic(this, &ThisClass::JoinButtonClicked);
+}
+
+void ULq_LANMenu::HostButtonClicked()
+{
+	FInputModeGameOnly InputMode;
+	GetOwningPlayer()->SetInputMode(InputMode);
+	GetOwningPlayer()->SetShowMouseCursor(false);
+
+	UGameplayStatics::OpenLevelBySoftObjectPtr(this, HostingLevel, true, TEXT("listen"));
+}
+
+void ULq_LANMenu::JoinButtonClicked()
+{
+	FInputModeGameOnly InputMode;
+	GetOwningPlayer()->SetInputMode(InputMode);
+	GetOwningPlayer()->SetShowMouseCursor(false);
+
+	const FString Address = TextBox_IpAddress->GetText().ToString();
+
+	UGameplayStatics::OpenLevel(this, *Address);
+}
---
+++ Source/LagQuest/Public/LAN/Lq_LANMenu.h
@@ -0,0 +1,38 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Lq_LANMenu.generated.h"
+
+class UButton;
+class UEditableTextBox;
+
+UCLASS()
+class LAGQUEST_API ULq_LANMenu : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeOnInitialized() override;
+
+private:
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UEditableTextBox> TextBox_IpAddress;
+
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UButton> Button_Host;
+
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UButton> Button_Join;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSoftObjectPtr<UWorld> HostingLevel;
+
+	UFUNCTION()
+	void HostButtonClicked();
+
+	UFUNCTION()
+	void JoinButtonClicked();
+};
