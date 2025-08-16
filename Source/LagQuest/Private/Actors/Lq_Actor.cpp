37f752a78d7d5efc5e01d692efe1e2d8ef625405
filliperomero <fillipe.romero@gmail.com>
2025-08-15 23:52:26 -0600

    Add replicated Actor

---
+++ Source/LagQuest/Private/Actors/Lq_Actor.cpp
@@ -0,0 +1,22 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#include "Actors/Lq_Actor.h"
+
+ALq_Actor::ALq_Actor()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	
+	bNetLoadOnClient = true;
+	bReplicates = true;
+	SetReplicatingMovement(true);
+}
+
+void ALq_Actor::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ALq_Actor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
---
+++ Source/LagQuest/Public/Actors/Lq_Actor.h
@@ -0,0 +1,22 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Lq_Actor.generated.h"
+
+UCLASS()
+class LAGQUEST_API ALq_Actor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ALq_Actor();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+};
