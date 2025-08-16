777d40fb4f5accd71fa43d868753b08a21ab3719
filliperomero <fillipe.romero@gmail.com>
2025-08-16 16:31:57 -0600

    Add Armor Actor to show replication in components

---
+++ Source/LagQuest/Private/Actors/Lq_Armor.cpp
@@ -0,0 +1,55 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#include "Actors/Lq_Armor.h"
+
+#include "Components/SphereComponent.h"
+#include "GameFramework/Character.h"
+
+ALq_Armor::ALq_Armor()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	bReplicates = true;
+
+	SetRootComponent(CreateDefaultSubobject<USceneComponent>("Root"));
+	
+	SphereMesh = CreateDefaultSubobject<UStaticMeshComponent>("SphereMesh");
+	SphereMesh->SetupAttachment(RootComponent);
+	SphereMesh->SetIsReplicated(true);
+
+	BootMesh_R = CreateDefaultSubobject<UStaticMeshComponent>("BootMeshR");
+	BootMesh_R->SetupAttachment(RootComponent);
+	BootMesh_R->SetIsReplicated(true);
+
+	BootMesh_L = CreateDefaultSubobject<UStaticMeshComponent>("BootMeshL");
+	BootMesh_L->SetupAttachment(RootComponent);
+	BootMesh_L->SetIsReplicated(true);
+	
+	SphereCollision = CreateDefaultSubobject<USphereComponent>("SphereCollision");
+	SphereCollision->SetupAttachment(RootComponent);
+}
+
+void ALq_Armor::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ALq_Armor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ALq_Armor::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+
+	ACharacter* OverlappedCharacter = Cast<ACharacter>(OtherActor);
+
+	if (HasAuthority() && IsValid(OverlappedCharacter))
+	{
+		// AttachToActor(OtherActor, FAttachmentTransformRules::SnapToTargetNotIncludingScale);
+		SphereMesh->AttachToComponent(OverlappedCharacter->GetMesh(), FAttachmentTransformRules::SnapToTargetNotIncludingScale, HeadSocket);
+		BootMesh_R->AttachToComponent(OverlappedCharacter->GetMesh(), FAttachmentTransformRules::SnapToTargetNotIncludingScale, RightFootSocket);
+		BootMesh_L->AttachToComponent(OverlappedCharacter->GetMesh(), FAttachmentTransformRules::SnapToTargetNotIncludingScale, LeftFootSocket);
+	}
+}
+
---
+++ Source/LagQuest/Public/Actors/Lq_Armor.h
@@ -0,0 +1,45 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Lq_Armor.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class LAGQUEST_API ALq_Armor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ALq_Armor();
+	virtual void Tick(float DeltaTime) override;
+	virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent> SphereMesh;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent> BootMesh_L;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent> BootMesh_R;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USphereComponent> SphereCollision;
+
+	UPROPERTY(EditAnywhere, Category = "Lag Quest")
+	FName HeadSocket { NAME_None };
+
+	UPROPERTY(EditAnywhere, Category = "Lag Quest")
+	FName LeftFootSocket { NAME_None };
+
+	UPROPERTY(EditAnywhere, Category = "Lag Quest")
+	FName RightFootSocket { NAME_None };
+};
