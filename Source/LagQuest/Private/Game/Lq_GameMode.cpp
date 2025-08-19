8c674b164aaef2f0918da3f2561e8081096c1853
filliperomero <fillipe.romero@gmail.com>
2025-08-18 21:11:54 -0600

    Add Game Mode

---
+++ Source/LagQuest/Private/Game/Lq_GameMode.cpp
@@ -0,0 +1,28 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+
+#include "Game/Lq_GameMode.h"
+
+ALq_GameMode::ALq_GameMode()
+{
+	bDelayedStart = true;
+}
+
+void ALq_GameMode::StartMatch()
+{
+	Super::StartMatch();
+
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Orange, "The match has started.");
+}
+
+void ALq_GameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	GetWorldTimerManager().SetTimer(MatchStartTimer, this, &ThisClass::StartDelayFinished, MatchStartDelay);
+}
+
+void ALq_GameMode::StartDelayFinished()
+{
+	StartMatch();
+}
---
+++ Source/LagQuest/Public/Game/Lq_GameMode.h
@@ -0,0 +1,32 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "Lq_GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LAGQUEST_API ALq_GameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	ALq_GameMode();
+
+	virtual void StartMatch() override;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	FTimerHandle MatchStartTimer;
+
+	UPROPERTY(EditDefaultsOnly)
+	float MatchStartDelay { 5.f };
+
+	void StartDelayFinished();
+};
